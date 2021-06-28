774a022f0db02ea8d5b3a8eeca2a093ab8e4012f
boyoon-c <boyoonchang@gmail.com>
2021-06-28 11:15:56 -0700

    make getGuess function

--- app.js
+++ app.js
@@ -2,10 +2,34 @@ const game = {
     title: 'Guess the Number!',
     biggestNum: 100,
     smallestNum: 1,
-    secretNum: Number,
+    secretNum: null,
+    prevGuesses: [],
+    getGuess: function(){
+        do {
+            //guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`)
+            //guess=parseInt(guess)
+            guess == this.secretNum
+
+            console.log(typeof guess)
+            if (typeof guess !== "string"){
+                console.log("Number works")
+                this.prevGuesses.push(guess)
+            } else {
+                console.log("Enter a number!")
+            }
+        } while (guess !== this.secretNum)
+    
+    },
+
     play: function(){
         this.secretNum = Math.floor(Math.random()*
-        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+            (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+        console.log(this.secretNum)
+        this.getGuess()
+        console.log(this.prevGuesses)     
+
     }
 }
+
+game.play()
 
